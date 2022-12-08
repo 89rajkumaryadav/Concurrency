3da41e478c1fc89ee3dcd502df51fa173a8dd5ff
rajkumaryadav89 <rajkumar.yadav@ecolane.com>
2022-12-08 13:00:06 +0530

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Rajkumar Yadav on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
